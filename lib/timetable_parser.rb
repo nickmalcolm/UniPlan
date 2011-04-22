9a4aadd2d73c894535e8ccc5b3801f2c6dda7711
Nick Malcolm <nick@southgatelabs.com>
2011-04-22 23:32:42 +1200

    Overhaul of parser to use hashes, just for fun. Phew, what a learning curve

--- lib/timetable_parser.rb
+++ lib/timetable_parser.rb
@@ -3,6 +3,24 @@ require 'open-uri'
 
 class TimetableParser
   
+  SCHEMA = {
+            :course => {
+                :course=>   'td[1]//text()',
+                :code=>     'td[2]//text()'
+            },
+            :stream => {
+              :crn=>      'td[3]//text()'
+            },
+            :event => {
+              :type=>     'td[4]//text()',
+              :dates=>    'td[5]//text()',
+              :days=>     'td[6]//text()',
+              :start=>    'td[7]//text()',
+              :finish=>   'td[8]//text()',
+              :room=>     'td[9]//text()'
+            }
+           }
+  
   class << self
     def parse(url = "")
       puts "Parsing Timetable...\n"
@@ -11,33 +29,47 @@ class TimetableParser
       doc = Nokogiri::HTML(f)
   
       rows = doc.xpath('//tr')
-  
-      details = rows.collect do |row|
-        detail = {}
-        [
-          [:course,   'td[1]//text()'],
-          [:code,     'td[2]//text()'],
-          [:crn,      'td[3]//text()'],
-          [:type,     'td[4]//text()'],
-          [:dates,    'td[5]//text()'],
-          [:days,     'td[6]//text()'],
-          [:start,    'td[7]//text()'],
-          [:finish,   'td[8]//text()'],
-          [:room,     'td[9]//text()'],
-        ].collect do |name, xpath|
-          detail[name] = row.at_xpath(xpath).to_s.strip
+      
+      #An array of all the courses
+      courses = []
+      
+      #For every row in the table
+      rows.collect do |row|
+        
+        #A hash of each course's attributes (relates to models)
+        attributes = {}
+        
+        #Use the schema on the row to get contents of a course attribute
+        SCHEMA.each do |attribute, children|
+          
+          attribute = attribute.to_sym
+          
+          #Keep track of the attribute's attributes!
+          child_hash = {}
+        
+          children.each do |child, xpath|
+            child = child.to_sym
+            text = row.at_xpath(xpath).to_s.strip
+          
+            #Create or update the hash
+            child_hash.update({child => text})
+          end
+          
+          attributes.update({attribute => child_hash})
         end
-        detail
+        courses << attributes
       end
-  
-      # y details[6][:crn]
-      #       y details[6][:crn].to_i
-      details.each do |d|
+      
+      
+      
+      courses.each do |course|
         begin
-          if d[:crn].to_i.eql? 0
+          #If the course doesn't have a CRN we don't care about it
+          if course[:stream][:crn].to_i.eql? 0
             next
           end
-          make_course(d)
+          #Make a course from the attributes
+          make_course(course)
         rescue ActiveRecord::RecordInvalid, NoMethodError => error
           p error
         end
@@ -46,41 +78,41 @@ class TimetableParser
       f.close
     end
   
-    def make_course(detail)
+    def make_course(course_attrs)
       
-      course = Course.find_or_create_by_course_and_code(:course => detail[:course], :code => detail[:code])
+      course = Course.find_or_create_by_course_and_code(
+                  :course => course_attrs[:course][:course], 
+                  :code => course_attrs[:course][:code])
       
-      stream = Stream.find_or_create_by_crn(detail[:crn].to_i)
+      stream = Stream.find_or_create_by_crn(course_attrs[:stream][:crn].to_i)
       stream.update_attributes(:course => course)
       
-      stream.events << make_events( stream, detail[:type], detail[:dates],
-                                    detail[:days], detail[:start], detail[:finish], 
-                                    detail[:room])
+      stream.events << make_events( stream, course_attrs[:event])
       
       course.streams << stream
       
       course
     end
                   
-    def make_events(stream, type, dates, day_initials, start, finish, room)
+    def make_events(stream, event_details)
       events = []
       
-      days = day_initials.split(//)
+      days = event_details[:days].split(//)
       
-      date = get_date(dates)
+      date = get_date(event_details[:dates])
       
       days.each do |d|
         date = add_day_of_week(date, d)
         
-        start_time = start.split(/:/)
+        start_time = event_details[:start].split(/:/)
         starts_at = date + start_time[0].to_i.hours + start_time[1].to_i.minutes
         
-        end_time = finish.split(/:/)
+        end_time = event_details[:finish].split(/:/)
         ends_at = date + end_time[0].to_i.hours + end_time[1].to_i.minutes
         
         event = Event.find_or_create_by_stream_id_and_starts_at(:stream=>stream, :starts_at => starts_at)
             
-        event.update_attributes(:ends_at => DateTime.now, :room => room)
+        event.update_attributes(:ends_at => ends_at, :room => event_details[:room])
             
         events << event
       end
