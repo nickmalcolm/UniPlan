ac1d5043f12a45aa282dae88e837525461ac2b18
Nick Malcolm <nick@southgatelabs.com>
2011-04-22 14:42:04 +1200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uniplan::Application.config.secret_token = 'd86494c2dfac84861b07ad898150e2bad26b71495919543686dcd97879d11154bafb6753ca12cdd7ab70072f7db778d75480022b0a08477b4f448f2aa16c240e'
