46d434a28cd715cbf075bd5d855301c76ee8bf47
Nick Malcolm <nick@southgatelabs.com>
2011-04-23 00:36:14 +1200

    Speeds up relationships

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ActiveRecord::Base
   
   has_many :streams, :dependent => :destroy
+  has_many :events, :through => :streams
   
   validates :course, :presence => true
   validates :code, :presence => true
